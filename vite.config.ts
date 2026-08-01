82ac5aa744a08e260d32d03e5f34c532056232eb
Harsh Panchal <harshpanchal@Harshs-MacBook-Air.local>
2026-08-02 01:40:05 +0530

    add deployment server

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vitest/config';
+
+const resolveAlias = (path: string) => new URL(path, import.meta.url).pathname;
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
-      '@shared': fileURLToPath(new URL('./src/shared', import.meta.url)),
-      '@features': fileURLToPath(new URL('./src/features', import.meta.url)),
+      '@app': resolveAlias('./src/app'),
+      '@shared': resolveAlias('./src/shared'),
+      '@features': resolveAlias('./src/features'),
     },
   },
   test: {
